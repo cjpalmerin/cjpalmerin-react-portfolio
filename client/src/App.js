739e9e86ea0f01645223ab8f3e6c0655da2d1c47
CJPALME <cjpalmerin@gmail.com>
2020-06-15 13:24:05 -0500

    added favicon and updated index.html

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src= "../assets/images/cjpalmerin_logo_500x500.png" class="logo" alt="cj palmerin logo" />
       <h1>Hello!</h1>
     </div>
   );
