81fae2aa500cbdaed7e67b3db52220ff3b13fba7
CJPALME <cjpalmerin@gmail.com>
2020-07-06 16:33:02 -0500

    transcribed resume

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -7,15 +7,99 @@ import Footer from '../components/Footer'
 export default function Resume() {
     return (
         <>
-        <Navbar />
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <img src='../assets/images/palmerinwanekCJ-resume_2020-02.png' alt="CJ Resume" id="resume" />
+            <Navbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div classname="resume">
+                            <div className="resume-header">
+                                <h3>Conner “CJ” Palmerin-Wanek</h3>
+                                <p>312-972-4182 • Chicago, IL 60611
+                        <br />
+Email: cjpalmerin@gmail.com
+<br />
+ GitHub: https://github.com/cjpalmerin  Portfolio: https://cjpalmerin-react.herokuapp.com/</p>
+                            </div>
+                            <h5>SUMMARY</h5>
+                            <p>I am a Front-end developer with a background in graphic design. Combining my technical knowledge in creating MERN applications with a history of cultivating a timely work ethic, strong communication and leadership skills, project management skills and an eye for design, I am an advantageous candidate for any team.</p>
+
+                            <br />
+                            <h5>TECHNICAL SKILLSs</h5>
+                            <p>Coding: JavaScript ES5/6 • HTML5 • CSS • React.js • Node.js • Express.js • MySQL • MongoDB • Third Party APIs
+<br />
+Design: Adobe Illustrator • Adobe Photoshop • Adobe Animate • Adobe Premiere Pro • iMovie • Final Cut Pro
+<br />
+Computers: Google Drive • Microsoft Word • Powerpoint • GitHub • Slack • Zoom • Skype • Social Media</p>
+
+<br />
+<h5>PROJECTS</h5>
+<p>ChromaCove | https://github.com/cjpalmerin/chroma-cove | https://afternoon-waters-22064.herokuapp.com/
+<br />
+Search for photography and add the photographer to your favorites for future reference after logging into the app. I was the project manager, worked with React on the front end, and connected the front to the back end.
+Technologies Used: React, Javascript, Passport, Node, Express, MongoDB, Mongoose, Unsplash API, Github, Materialize CSS, UI Kit.
+<br />
+<br />
+LocalSpot | https://github.com/blapete/LocalSpot |: https://agile-journey-25400.herokuapp.com/
+<br />
+Check out LocalSpot for a curated selection of locations around town. Visitors and Chicago natives alike could find a new spot to check out or see a familiar spot they love and drop a like! I was in charge of the front end and worked on routing on the backend. Technologies Used: Javascript, Node, Express, Express-Handlebars, MySQL2, Sequelize, Yelp API, Materialize CSS, Github
+<br />
+<br />
+weatherEats | https://github.com/cbruder1292/Project1 | https://cbruder1292.github.io/Project1/
+<br />
+weatherEats accepts city names as the user input and suggests a meal recipe based on the temperature . I was in charge of the front end of the application. I designed and coded the UI. Technologies Used: HTML5, CSS, Javascript, Github, Open Weather API, TheMealDB API, Materialize CSS
+</p>
+<br />
+<h5>EXPERIENCE</h5>
+Graphic Designer - Current App / Current Network
+<br />
+June 2019 - March 2020
+<br />
+Designed brand identity for CURRENT NETWORK, created static and animated marketing collateral for Current Music App. Generated marketing oriented content for social media pages owned by Current. Utilized timely work ethic, strong communication and client service skills to complete projects.
+<br />
+<br />
+Graphic Designer and Animator - PVTSTCK (Private Stock)
+<br />
+March 2019 - October 2019
+<br />
+Designed single covers for streaming services, animated social media content for individual artists' music, and designed brand identity for INTAKE show hosted by PVTSTCK. Utilized timely work ethic, strong communication, project management, and client service skills to complete projects.
+<br />
+<br />
+Graphic Designer, Web Designer - B. Michelle Beauty
+<br />
+June 2018 - March 2020
+<br />
+Designed brand identity and created website via Wix. Generated print and digital marketing collateral. Utilized timely work ethic, strong communication, leadership, and client service skills to complete projects.
+<br />
+<br />
+Graphic Designer, Web Designer, Video Editor - &blowdry full service salon
+<br />
+January 2018 - January 2020
+<br />
+Created the brand identity and designed all print and digital marketing collateral for Chicago, IL and Lawrence, KS locations. Designed and created the entire website via Wix. Utilized timely work ethic, strong communication, and client service skills to complete projects.
+<br />
+<br />
+Rags of Honor/ROH + Veteran Roasters - Graphic & Product designer
+<br />
+September 2016 - April 2018
+<br />
+Designed product and marketing designs for the main Rags of Honor brand, ROH and Verteran Roasters. Collaborated on designs with multiple brands like the Chicago Blackhawks and Steve Cochran of WGN. Utilized timely work ethic, strong communication, and client service skills to complete projects.
+<br />
+<br />
+<h5>EDUCATION</h5>
+Northwestern University Full-Time Full Stack Web Development Bootcamp
+<br />
+Received Certificate of Completion
+<br />
+<br />
+DePaul University
+<br />
+BA Public Relations & Advertising, Graphic Design Minor
+
+                </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     )
 }
